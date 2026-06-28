34c161d123c6f55d7961c9ed35411595d698953d
Stephen Henstrom <132288391+stevehenny@users.noreply.github.com>
2026-06-28 01:36:23 -0700

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> dp(n, 1);
+
+        for(int i{0}; i < n; ++i){
+            for(int j{i+1}; j < n; ++j){
+                if(nums[j] > nums[i]) dp[j] = std::max(dp[j], dp[i] + 1);
+            }
+        }
+        for(int i{}; i < n; ++i){
+            std::cout << dp[i] <<' ';
+        }
+   
+        return std::ranges::max(dp);
+    }
+};
