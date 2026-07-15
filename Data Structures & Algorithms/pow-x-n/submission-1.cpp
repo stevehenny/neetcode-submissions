a54d97e64e8a1dec97901192d3e60699848c91fb
Stephen Henstrom <132288391+stevehenny@users.noreply.github.com>
2026-07-14 21:36:50 -0700

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    unordered_map<int, double> memo;
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == 2) return x * x;
+        
+        int na = abs(n);
+        double ans;
+        if(n % 2 == 0){
+            ans = myPow(myPow(x, na/2), 2);
+        }
+        else{
+            ans = x*myPow(myPow(x, na/2), 2);
+        }
+
+        if(n > 0){
+            return ans;
+        }
+        else{
+            return 1/ans;
+        }
+
+        
+    }
+};
