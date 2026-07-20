77b310e9e42bcdc8578b25166a6d31e837ee5ac6
Stephen Henstrom <132288391+stevehenny@users.noreply.github.com>
2026-07-19 22:37:02 -0700

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+
+        //init adj_list
+        unordered_map<int, vector<pair<int,int>>> adj_list;
+        std::cout << "INIT FLIGHTS" << '\n';
+        for(auto& flight: flights){
+            adj_list[flight[0]].push_back({flight[1], flight[2]});
+            // std::cout << "(" << flight[0] << ',' << flight[1] << ")->" << flight[2] << '\n';
+        }
+        
+        // init dist and pq
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+        
+        // [cost, u, cur_k];
+        priority_queue<tuple<int,int,int>,
+                        vector<tuple<int,int,int>>,
+                        std::greater<>> pq;
+
+        pq.push(tuple<int,int,int>{0, src, 0});
+
+        while(!pq.empty()){
+            auto [cost, u, cur_k] = pq.top();
+            pq.pop();
+
+            for(auto& [v, w]: adj_list[u]){
+                int new_cost = w + cost;
+
+                if(cur_k >= k && v != dst) continue;
+
+                if(new_cost < dist[v]){
+                    dist[v] = w + cost;
+                    pq.push(tuple<int,int,int>{w + cost, v, cur_k + 1});
+                }
+            }
+        }
+
+  
+        return (dist[dst] == INT_MAX) ? -1 : dist[dst];
+    }
+};
